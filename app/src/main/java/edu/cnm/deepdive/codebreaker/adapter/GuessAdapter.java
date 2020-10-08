4e2583f407b7e3fb54f968170bb171028a8e06cd
jkelly97 <jmkelly2041@gmail.com>
2020-10-08 15:02:48 -0500

    Implemented view binding.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/adapter/GuessAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/adapter/GuessAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import edu.cnm.deepdive.codebreaker.R;
+import edu.cnm.deepdive.codebreaker.databinding.ItemGuessBinding;
 import edu.cnm.deepdive.codebreaker.model.Code.Guess;
 import java.util.ArrayList;
 import java.util.Map;
@@ -30,26 +31,23 @@ public class GuessAdapter extends ArrayAdapter<Guess> {
   @NonNull
   @Override
   public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-    View layout = (convertView != null)
-        ? convertView
-        : inflater.inflate(R.layout.item_guess, parent, false);
+    ItemGuessBinding binding= (convertView != null)
+        ? ItemGuessBinding.bind(convertView)
+        : ItemGuessBinding.inflate(inflater, parent, false);
     Guess guess = getItem(position);
-    TextView guessNumber = layout.findViewById(R.id.guess_number);
-    TextView correct = layout.findViewById(R.id.correct);
-    TextView close = layout.findViewById(R.id.close);
-    guessNumber.setText(String.valueOf(position + 1));
+    binding.guessNumber.setText(String.valueOf(position + 1));
     //noinspection ConstantConditions
-    correct.setText(String.valueOf(guess.getCorrect()));
-    close.setText(Integer.toString(guess.getClose()));
-    LinearLayout guessContainer = layout.findViewById(R.id.guess_container);
-    guessContainer.removeAllViews();
+    binding.correct.setText(String.valueOf(guess.getCorrect()));
+    binding.close.setText(Integer.toString(guess.getClose()));
+    binding.guessContainer.removeAllViews();
     for (char c : guess.getText().toCharArray()) {
-      ImageView swatch = (ImageView) inflater.inflate(R.layout.item_swatch, guessContainer, false);
+      ImageView swatch =
+          (ImageView) inflater.inflate(R.layout.item_swatch, binding.guessContainer, false);
       swatch.setBackgroundColor(colorMap.get(c));
       swatch.setContentDescription(String.valueOf(c));
-      guessContainer.addView(swatch);
+      binding.guessContainer.addView(swatch);
     }
-    return layout;
+    return binding.getRoot();
   }
 
 }
